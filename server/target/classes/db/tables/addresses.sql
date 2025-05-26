639d39c46f0f5daf7d7737d50ea85c26c57cac2a
sergio <sergioenricopisoni@gmail.com>
2025-05-26 17:48:18 +0200

    aggiunto .gitignore per non salvare i binari, speriamo in Dio

--- server/target/classes/db/tables/addresses.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS Addresses (
-    address_id SERIAL PRIMARY KEY,
-    country VARCHAR(50) NOT NULL,
-    city VARCHAR(168) NOT NULL,
-    street VARCHAR(255) NOT NULL,
-    house_number VARCHAR(63),
-    CONSTRAINT unique_address UNIQUE(country, city, street, house_number)
-);
